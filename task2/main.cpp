a5ad5a7c5c36e38870596aa45356d60d7c2a07d4
Sanchozz <knopochnik@mail.ru>
2020-10-10 09:27:56 +0300

    format with universal reference param removed

--- task2/main.cpp
+++ task2/main.cpp
@@ -5,18 +5,72 @@
 #include <tuple>
 #include <sstream>
 
-
 struct Printer {
 public:
     std::string str() const {
         return buffer.str();
     }
 
-    template<typename T>
-    Printer& format(T&& object) {
-        format_helper(object);
+    // tuple printer
+    template<std::size_t...> struct seq{};
+
+    template<std::size_t N, std::size_t... Is>
+    struct gen_seq : gen_seq<N-1, N-1, Is...>{};
+
+    template<std::size_t... Is>
+    struct gen_seq<0, Is...> : seq<Is...>{};
+
+    template<class Tuple, std::size_t... Is>
+    void tuple_printer(const Tuple &t, seq<Is...>) {
+        int tuple_iterator[] = {0, (buffer << (Is == 0? "" : ", "), format(std::get<Is>(t)), 42)...};
+        (void)tuple_iterator;
+    }
+
+    // vector printer
+    template<typename T> 
+    Printer& format(const std::vector<T> &v) {
+        buffer << "[ ";
+        format_iterable(v);
+        buffer << " ]";
         return *this;
     }
+
+    // set printer
+    template<typename T> 
+    Printer& format(const std::set<T> &s) {
+        buffer << "{ ";
+        format_iterable(s);
+        buffer << " }";
+        return *this;
+    }
+
+    // pair printer
+    template<typename T1, typename T2> 
+    Printer& format(const std::pair<T1, T2> &p) {
+        buffer << "(";
+        format(p.first);
+        buffer << ", ";
+        format(p.second);
+        buffer << ")";
+        return *this;
+    }
+
+    // tuple printer
+    template<typename ...Args> 
+    Printer& format(const std::tuple<Args...> &t) {
+        buffer << "( ";
+        tuple_printer(t, gen_seq<sizeof...(Args)>());
+        buffer << " )";
+        return *this;
+    }
+
+    // default printer
+    template<typename T>
+    Printer& format(const T& element) {
+        buffer << element;
+        return *this;
+    }
+    
 private:
     // iterable containter printer (set and vector in this case)
     template<typename T>
@@ -33,61 +87,7 @@ private:
             format(*begin_it++);
         }
     }
-
-    // tuple printing
-    template<std::size_t...> struct seq{};
-
-    template<std::size_t N, std::size_t... Is>
-    struct gen_seq : gen_seq<N-1, N-1, Is...>{};
-
-    template<std::size_t... Is>
-    struct gen_seq<0, Is...> : seq<Is...>{};
-
-    template<class Tuple, std::size_t... Is>
-    void tuple_printer(const Tuple &t, seq<Is...>) {
-        int tuple_iterator[] = {0, (buffer << (Is == 0? "" : ", "), format_helper(std::get<Is>(t)), 42)...};
-        (void)tuple_iterator;
-    }
-
-    // vector printer
-    template<typename T> 
-    void format_helper(const std::vector<T> &v) {
-        buffer << "[ ";
-        format_iterable(v);
-        buffer << " ]";
-    }
-
-    // set printer
-    template<typename T> 
-    void format_helper(const std::set<T> &s) {
-        buffer << "{ ";
-        format_iterable(s);
-        buffer << " }";
-    }
-
-    // pair printer
-    template<typename T1, typename T2> 
-    void format_helper(const std::pair<T1, T2> &p) {
-        buffer << "(";
-        format_helper(p.first);
-        buffer << ", ";
-        format_helper(p.second);
-        buffer << ")";
-    }
-
-    // tuple printer
-    template<typename ...Args> 
-    void format_helper(const std::tuple<Args...> &t) {
-        buffer << "( ";
-        tuple_printer(t, gen_seq<sizeof...(Args)>());
-        buffer << " )";
-    }
-
-    // default printer
-    template<typename T>
-    void format_helper(const T& element) {
-        buffer << element;
-    }
+    
 private:
     std::stringstream buffer;
 };
